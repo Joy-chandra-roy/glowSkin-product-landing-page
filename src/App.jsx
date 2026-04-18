acaefb80229e258b436e1809404f159c356cde84
joy <joyr43004@gmail.com>
2026-04-18 17:00:17 +0600

    tailwind and framer-motion install and setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { easeInOut, motion } from "motion/react"
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='mx-20'>
+
+      <motion.h1 
+        initial={{opacity:0, y:50}}
+        animate={{opacity:1, y:0}}
+        transition={{duration:1}}
+      className='text-5xl text-purple-700 font-bold text-center shadow shadow-blue-300 pb-3'>
+        Hello Everybody, Good Afternoon
+      </motion.h1>
+
     </div>
   );
 };
